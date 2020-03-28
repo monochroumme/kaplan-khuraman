6d1d336f461b724525fc5770c154b423adebbe8c
rare <chipchroumme@gmail.com>
2020-03-28 16:30:25 +0400

    gulp works now, header part is done (not adaptive), section 1 is almost done (no slider and not adaptive)

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// https://goede.site/setting-up-gulp-4-for-automatic-sass-compilation-and-css-injection
+
+const gulp = require("gulp"),
+      sass = require("gulp-sass"),
+      postcss = require("gulp-postcss"),
+      autoprefixer = require("autoprefixer"),
+      cssnano = require("cssnano"),
+      sourcemaps = require("gulp-sourcemaps"),
+      browserSync = require("browser-sync").create();
+
+const paths = {
+  styles: {
+    src: "./res/scss/*.scss",
+    dest: "res/css"
+  }
+}
+
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            // Initialize sourcemaps before compilation starts
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            // Use postcss with autoprefixer and compress the compiled file using cssnano
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            // Now add/write the sourcemaps
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+    );
+}
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch([paths.styles.src, "./*.html", "./**/.svg", "./**/.png"], gulp.series(style, reload));
+}
+
+exports.default = gulp.series(watch);
